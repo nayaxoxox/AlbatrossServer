b2bc53619c74027006887cba52c5b28cd3db6eb8
QingWan <1109162935@qq.com>
2025-08-29 06:13:08 +0800

    Add test code.

---
+++ test/launch_test.py
@@ -0,0 +1,29 @@
+import time
+
+import albatross
+from albatross.common import Configuration
+
+
+def main(device_id=None):
+  device = albatross.get_device(device_id)
+  assert device.is_root
+  device.wake_up()
+  device.home()
+  user_pkgs = device.get_user_packages()
+  inject_dex = Configuration.resource_dir + "plugins/injector_demo.apk"
+  inject_class = "qing/albatross/app/agent/DemoInjector"
+  for pkg in user_pkgs:
+    if 'albatross' in pkg and 'inject_demo' not in pkg:
+      continue
+    print('try test', pkg)
+    device.launch(pkg, inject_dex, None, inject_class)
+    time.sleep(2)
+    for i in range(3):
+      device.switch_app()
+      time.sleep(1)
+      device.switch_app()
+  print('finish test')
+
+
+if __name__ == '__main__':
+  main()
